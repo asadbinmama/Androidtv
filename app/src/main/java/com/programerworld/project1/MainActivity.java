eb0c1c43ce93698e573a8e9210369f6f3705220a
asad <asadbinmama@gmail.com>
2024-05-13 14:34:00 +0700

    project tv

--- app/src/main/java/com/programerworld/project1/MainActivity.java
+++ app/src/main/java/com/programerworld/project1/MainActivity.java
@@ -1,76 +1,39 @@
+
 package com.programerworld.project1;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText numCodeEditText;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // เริ่มต้น EditText
-        numCodeEditText = findViewById(R.id.numcode);
-
-        // เพิ่ม TextChangedListener เพื่อตรวจสอบจำนวนตัวอักษรที่ถูกป้อนใน EditText
-        numCodeEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ก่อนที่ข้อความจะเปลี่ยนแปลง
             }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // เมื่อข้อความถูกเปลี่ยนแปลง
-            }
-            @Override
-            public void afterTextChanged(Editable s) {
-                // หลังจากที่ข้อความถูกเปลี่ยนแล้ว
-
-                // ตรวจสอบว่าข้อความมีความยาวมากกว่า 5 ตัวหรือไม่
-                if (s.length() > 5) {
-                    // ถ้ามีมากกว่า 5 ตัว ให้ลบตัวอักษรที่เกินกว่า 5 ตัว
-                    s.delete(5, s.length());
-                }
-            }
-        });
-    }
-
-    // วิธีการที่เรียกว่าเมื่อคลิกปุ่ม "Next"
-    public void openWebsite(View view) {
-        // รับรหัสที่ป้อนจาก EditText
+    public void actimepray(View view) {
+        // สร้าง Intent เพื่อเปิด activity timepray
+        EditText numCodeEditText = findViewById(R.id.numcode);
         String numCode = numCodeEditText.getText().toString();
-
-        // ตรวจสอบว่ารหัสที่ป้อนถูกต้องหรือไม่ (ในกรณีนี้คือรหัส 5 หลัก)
-        if (numCode.length() == 5) {
-            // รหัสถูกต้อง สร้าง URL ด้วยรหัสที่ป้อน
-            String formattedCode = numCode.substring(0, 2) + "." + numCode.substring(2);
-            String url = "https://imacscenter.org/masjid-prayer-time/" + formattedCode;
-
-            // เปิด URL ในเว็บเบราว์เซอร์
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-        } else {
-            // รหัสไม่ถูกต้อง แสดงข้อความแสดงข้อผิดพลาด
-            Toast.makeText(this, "กรุณาป้อนข้อมูลให้ครบ 5 ตัว", Toast.LENGTH_SHORT).show();
+        // ตรวจสอบว่า numCode มีความยาวเท่ากับ 5 หรือไม่
+        if (numCode.length() != 5) {
+            // แสดงข้อความแจ้งเตือนให้กรอกรหัสมัสยิดให้ครบ 5 ตัว
+            Toast.makeText(this, "กรุณากรอกรหัสมัสยิดให้ครบ 5 ตัว", Toast.LENGTH_SHORT).show();
+            return; // ออกจากเมธอด
         }
+        // ถ้า numCode มีความยาวเท่ากับ 5 ให้ดำเนินการต่อไปโดยปกติ
+        // สร้าง Intent เพื่อเปิด activity timepray
+        Intent intent = new Intent(MainActivity.this, timepray.class);
+        startActivity(intent); // เริ่ม activity timepray
     }
-
-    // วิธีการเปิดนโยบายความเป็นส่วนตัว
     public void openPrivacyPolicy(View view) {
         // Open the privacy policy URL in a web browser
         String privacyPolicyUrl = "https://imacscenter.org/main/privacy-policy/";
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(privacyPolicyUrl));
         startActivity(intent);
-
-
     }
 }
--- app/src/main/java/com/programerworld/project1/timepray.java
+++ app/src/main/java/com/programerworld/project1/timepray.java
@@ -1,13 +1,35 @@
 package com.programerworld.project1;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class timepray extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timepray);
 
+        final TextView textView = findViewById(R.id.time);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss", Locale.US);
+                String currentTime = sdf.format(new Date());
+                textView.setText(currentTime);
+                handler.postDelayed(this, 0); // อัปเดตทุกๆ 1 วินาที
+            }
+            //ssss
+        });
+
+        final TextView textViewDate = findViewById(R.id.Date);
+        SimpleDateFormat sdfDate = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.US);
+        String currentDate = sdfDate.format(new Date());
+        textViewDate.setText(currentDate);
 
     }
 }
